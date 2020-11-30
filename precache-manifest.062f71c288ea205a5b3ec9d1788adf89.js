8ed85d0fa02bad2c53cd58ab8beacd3ab9b98c37
EmanuelOne <emanuel4mechie@gmail.com>
2020-11-30 21:33:34 +0100

    Updates

---
+++ precache-manifest.062f71c288ea205a5b3ec9d1788adf89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20de6977040a81a433cc44234a6c8e54",
+    "url": "/MusicApp/index.html"
+  },
+  {
+    "revision": "a362ff58cff7afc88c39",
+    "url": "/MusicApp/static/css/main.b41526bd.chunk.css"
+  },
+  {
+    "revision": "9ce6e1f6eb65fd6b3bc1",
+    "url": "/MusicApp/static/js/2.404e5c49.chunk.js"
+  },
+  {
+    "revision": "a63320ff7dda6e4506aa7e582b560ec1",
+    "url": "/MusicApp/static/js/2.404e5c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a362ff58cff7afc88c39",
+    "url": "/MusicApp/static/js/main.0a1ea397.chunk.js"
+  },
+  {
+    "revision": "bdd74520d93baf9575bd",
+    "url": "/MusicApp/static/js/runtime-main.82b7247c.js"
+  }
+]);
